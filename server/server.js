90bb35301251a0af62757b38fc1d69f0f18b355b
Mahak <56362610+Mahak-G@users.noreply.github.com>
2024-03-24 22:22:58 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use(cors());
 // }));
 app.use(bodyParser.json({extended: true}));
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use('/',router);
 const PORT= 8000;
 
 const USERNAME= process.env.DB_USERNAME;
